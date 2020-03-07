a11695de9b7bafe80fb5195933ebeeab20a234b1
George Pt <angry_sephiroth@hotmail.com>
2020-03-07 19:30:03 +0200

    Update one-piece-manga.py
    Previous version saved files as "1-17" so new files replaced old ones.
    Fixed this by changing the saved file name.

--- one-piece-manga.py
+++ one-piece-manga.py
@@ -3,7 +3,9 @@
 
 import requests, os, bs4
 
-url = 'https://read-onepiece.com/manga/one-piece-chapter-973/'               # starting url
+#TODO: how to handle server error without crushing -> reload page and try again, if not, go to next page
+
+url = 'https://read-onepiece.com/manga/one-piece-chapter-966/'               # starting url
 os.makedirs('One Piece', exist_ok=True)    # store comics in ./lfg  no exception if folder already exists
 while url != 'https://read-onepiece.com/manga/onepiece-chapter-1/': #careful, layout & URL changes to one-piece-chapter-x
     # Download the page.
@@ -15,8 +17,6 @@ while url != 'https://read-onepiece.com/manga/onepiece-chapter-1/': #careful, la
 
     # Find the URL of the comic image.
     comicElem = soup.select('.separator img') # <div id="comic-img"> <img src="xxx"> ---> Elem = soup.select('#id element')
-    # print(comicElem)
-    # print(len(comicElem))
     if comicElem == []: # if selector does not find any elements, it returns empty list 
         print('Could not find comic image.')
     else:
@@ -28,14 +28,23 @@ while url != 'https://read-onepiece.com/manga/onepiece-chapter-1/': #careful, la
             res.raise_for_status()  
 
             # Save the image to ./One Piece.
-            imageFile = open(os.path.join('One Piece', os.path.basename(comicUrl)),
-    'wb') # call os.path.basename() with comicUrl, and it will return just the last part of the URL, 'heartbleed_explanation.png' /// join for Windows & Linux
+            fileName = url[-4:-1] + '_' + str(i+1)
+            print(fileName)
+            imageFile = open('.\\One Piece\\%s.jpg' % fileName,
+    'wb')
             for chunk in res.iter_content(100000):
                 imageFile.write(chunk)
             imageFile.close()
 
         # Get the Prev button's url.
         prevLink = soup.select('a[rel="prev"]')[0] # selector 'a[rel="prev"]' identifies the <a> element with the rel attribute set to prev
-        url = prevLink.get('href') # next page = none
+        url = prevLink.get('href')
 
 print('Done.')
+
+# chapter 967 ---> photos do not load in site
+# raise HTTPError(http_error_msg, response=self)
+# requests.exceptions.HTTPError: 403 Client Error: Forbidden
+# for url: https://1.bp.blogspot.com/-mFxjKPPC5yw/XhC24wPfPRI/AAAAAAAAi_E/CUfY0zjJ8EgVbicJ0f2HqXlmQXRqAQWngCLcBGAsYHQ/s1600/onepiece1.jpg
+
+# up to 940 runs ok
