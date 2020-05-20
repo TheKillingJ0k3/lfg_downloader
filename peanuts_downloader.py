65ef49c5eedfc0764dc3fe8addebade3a4f4f5b2
George Pt <angry_sephiroth@hotmail.com>
2020-05-20 03:02:52 +0300

    Refactored
    Refactored code
    Created functions 'choose_year' and 'OneDrive_upload'

--- peanuts_downloader.py
+++ peanuts_downloader.py
@@ -1,29 +1,49 @@
 #! python3
 # peanuts_downloader.py - Downloads every single Peanuts comic.
 
-# https://www.gocomics.com/little-moments-of-love/2018/09/24
-# https://www.gocomics.com/cestlavie/2020/05/08
-# https://www.gocomics.com/the-adventures-of-business-cat/2020/05/07
-# https://www.gocomics.com/calvinandhobbes/2020/05/10
-# https://www.gocomics.com/peanuts/2020/05/10
-
-import requests, bs4
+# Standard library imports
 import shutil, os, subprocess
+# Third party imports
+import requests, bs4
+# Local app imports
+# import Tkinter_GUI_Skeleton
 
 # url = 'https://www.gocomics.com/peanuts/1950/10/02'               # starting url
 # url = 'https://www.gocomics.com/peanuts/1984/01/01'               # starting url
 
+#GLOBAL VARIABLES
+year = ''
+url = ''
+OneDriveUpload = ''
+#####################################################
+
+##################################  FUNCTIONS  ##################################################
+def choose_year():
+    global year
+    global url
+    year = input('Pick a year between 1950 and current year:') # year 1950 starts from 02/10
+    if year == '1950':
+        url = 'https://www.gocomics.com/peanuts/1950/10/02' # starting url for 1950
+    else:
+        url = 'https://www.gocomics.com/peanuts/{}/01/01' .format(year) # starting url
+
+
+def OneDrive_upload():
+    global OneDriveUpload
+    OneDriveUpload = input('Do you want to upload your newly created folder to Onedrive? \nPress: Y for Yes, N for No: ')
+    if OneDriveUpload == 'Y':
+        shutil.copytree(os.path.join('Peanuts', year), os.path.join('OneDrive', 'Comics', 'Peanuts', year))
+        subprocess.Popen(os.path.join('AppData', 'Local', 'Microsoft', 'OneDrive', 'OneDrive.exe'))
+    elif OneDriveUpload == 'N':
+        pass
+    else:
+        print('I SAID Press: Y for Yes, N for No')
+#############################################################################################
 
 os.makedirs('Peanuts', exist_ok=True)    # store comics in ./Peanuts  no exception if folder already exists
 
-year = input('Pick a year between 1950 and current year:') # year 1950 starts from 02/10
-if year == '1950':
-    url = 'https://www.gocomics.com/peanuts/1950/10/02' # starting url for 1950
-else:
-    url = 'https://www.gocomics.com/peanuts/{}/01/01' .format(year) # starting url
+choose_year()
 
-
-# URLyear = str(url[-10:-6])
 while year in str(url[-10:-6]): # loop condition: year that user picked should be in URL
     os.makedirs(os.path.join('Peanuts', year), exist_ok=True)
 
@@ -32,7 +52,6 @@ while year in str(url[-10:-6]): # loop condition: year that user picked should b
     res = requests.get(url)
     res.raise_for_status()
 # 1967.04.17 requests.exceptions.HTTPError: 502 Server Error: Bad Gateway for url: https://assets.amuniversal.com/b7ac0120f892013014ff001dd8b71c47
-
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     # Find the URL of the comic image.
@@ -52,8 +71,6 @@ while year in str(url[-10:-6]): # loop condition: year that user picked should b
         # Save the image to ./Peanuts/Year.
         imageFile = open(os.path.join('Peanuts', year, os.path.basename(filename + '.jpg')), # comicUrl does not end with .jpg, so we add it manually
 'wb') # call os.path.basename() with comicUrl, and it will return just the last part of the URL /// join for Windows & Linux
-
-
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
@@ -62,15 +79,6 @@ while year in str(url[-10:-6]): # loop condition: year that user picked should b
     prevLink = soup.select('a[class="fa btn btn-outline-secondary btn-circle fa-caret-right sm"]')[0]
     url = 'https://www.gocomics.com/' + prevLink.get('href')
 
-    # URLyear = str(url[-10:-6])
-
 print('Done.')
 
-OneDriveUpload = input('Do you want to upload your newly created folder to Onedrive? \nPress: Y for Yes, N for No: ')
-if OneDriveUpload == 'Y':
-    shutil.copytree(os.path.join('Peanuts', year), os.path.join('OneDrive', 'Comics', 'Peanuts', year))
-    subprocess.Popen(os.path.join('AppData', 'Local', 'Microsoft', 'OneDrive', 'OneDrive.exe'))
-elif OneDriveUpload == 'N':
-    pass
-else:
-    print('I SAID Press: Y for Yes, N for No')
+OneDrive_upload()
