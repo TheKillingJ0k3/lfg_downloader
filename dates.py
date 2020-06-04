cf1543147796aa562f1b72369d0d663eb752c4e8
George Pt <angry_sephiroth@hotmail.com>
2020-06-04 15:48:37 +0300

    Update dates.py

--- dates.py
+++ dates.py
@@ -10,7 +10,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 from time import sleep
 
+back_months = {'January': '01', 'February': '02', 'March':'03', 'April':'04',
+            'May':'05', 'June':'06', 'July':'07', 'August':'08',
+            'September':'09', 'October':'10', 'November':'10', 'December':'12'}
 
+months = {'01': 'January', '02': 'February', '03': 'March', '04': 'April',
+            '05': 'May', '06': 'June', '07': 'July', '08': 'August',
+            '09': 'September', '10': 'October', '11': 'November', '12': 'December'}
 ###############################################################################################
 def getIndexPositions(listOfElements, element):
     ''' Returns the indexes of all occurrences of give element in
@@ -32,6 +38,7 @@ def getIndexPositions(listOfElements, element):
 def download_comic_with_Selenium():
     Dates = []
     # selenium method with headless browser
+    #(global first publication url)
     url = 'https://www.gocomics.com/the-adventures-of-business-cat/2020/05/28'
     # chrome_options = Options()
     # chrome_options.add_argument('--headless')
@@ -41,7 +48,12 @@ def download_comic_with_Selenium():
 
     # while True:
     # find element
-    continue_banner = browser.find_element_by_xpath('/html/body/div[10]/div[1]/div/div/div[4]/button[2]') # /html/body/div[10]/div[1]/div/div/div[4]/button[2]
+    try:
+        continue_banner = browser.find_element_by_xpath('/html/body/div[10]/div[1]/div/div/div[4]/button[2]')
+    except:
+        sleep(10)
+        continue_banner = browser.find_element_by_xpath('/html/body/div[10]/div[1]/div/div/div[4]/button[2]')
+
     continue_banner.click()
     cookies_banner = browser.find_element_by_xpath('//*[@id="cookieChoiceDismiss"]')
     cookies_banner.click() 
@@ -52,15 +64,15 @@ def download_comic_with_Selenium():
         actions.send_keys(Keys.SPACE).perform()
         sleep(5)
 
-    calendar_button = browser.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div/div[1]/nav/div[2]/div/input') # calendar button
+    calendar_button = browser.find_element_by_xpath('/html/body/div[3]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div/div[1]/nav/div[2]/div/input')
     calendar_button.click() # ElementClickInterceptedException
     sleep(2)
     select_year_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[2]/select')
     select_year = Select(select_year_button)
     sleep(2)
     # select by visible text
-    #####
-    res = requests.get(url) # check if we have problems with global var
+    ##### - first publication url global var so this is not needed inside program
+    res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     # find button for first comic page
@@ -77,81 +89,157 @@ def download_comic_with_Selenium():
     # year = '2015'
     select_year.select_by_visible_text(year)
     sleep(1)
-    # for i in range(12)?
-    select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
-    select_month = Select(select_month_button)
-    sleep(2)
-    # select by visible text
-    try:
-        select_month.select_by_visible_text('January')
-    except:
-        try:
-            select_month.select_by_visible_text('February')
-        except:
+    while int(month) <13:
+        select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
+        select_month = Select(select_month_button)
+        sleep(2)
+        # select by visible text
+        letter_month = months.get(month)
+        print(letter_month)
+        select_month.select_by_visible_text(letter_month)
+        # try:
+        #     select_month.select_by_visible_text('January')
+        #     month = 'January'
+        # except:
+        #     try:
+        #         select_month.select_by_visible_text('February')
+        #         month = 'February'
+        #     except:
+        #         try:
+        #             select_month.select_by_visible_text('March')
+        #             month = 'March'
+        #         except:
+        #             try:
+        #                 select_month.select_by_visible_text('April')
+        #                 month = 'April'
+        #             except:
+        #                 try:
+        #                     select_month.select_by_visible_text('May')
+        #                     month = 'May'
+        #                 except:
+        #                     try:
+        #                         select_month.select_by_visible_text('June')
+        #                         month = 'June'
+        #                     except:
+        #                         try:
+        #                             select_month.select_by_visible_text('July')
+        #                             month = 'July'
+        #                         except:
+        #                             try:
+        #                                 select_month.select_by_visible_text('August')
+        #                                 month = 'August'
+        #                             except:
+        #                                 try:
+        #                                     select_month.select_by_visible_text('September')
+        #                                     month = 'September'
+        #                                 except:
+        #                                     try:
+        #                                         select_month.select_by_visible_text('October')
+        #                                         month = 'October'
+        #                                     except:
+        #                                         try:
+        #                                             select_month.select_by_visible_text('November')
+        #                                             month = 'November'
+        #                                         except:
+        #                                             select_month.select_by_visible_text('December')
+        #                                             month = 'December'
+                                                
+        sleep(5)
+        print(month)
+        # if letter_month in back_months.keys():
+        #     month = back_months.get(letter_month)
+        #     print(month)
+        dates_elements = browser.find_elements_by_class_name('ui-state-default')
+
+        # print(dates_elements)
+        print(len(dates_elements))
+        month_dates_yayornay = []
+
+        for i in dates_elements:
+            x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a //////find_element_by_css_selector('a').
+            print(x)
+            month_dates_yayornay.append(x)
+        # for x in month_dates_yayornay:
+        #     if x:
+        #         Dates.append(year + '-' + month + '-' + str(int(month_dates_yayornay.index(x))+1))
+        # print(Dates)
+
+
+        # index only finds first occurence of value that's why I only get 0 and 3
+        indexPosList = []
+        indexPos = 0
+        while True:
             try:
-                select_month.select_by_visible_text('March')
-            except:
-                select_month.select_by_visible_text('April')
-    sleep(5)
-    month = ''
-    #TODO: see how to save month in variable to turn to number
-    dates_elements = browser.find_elements_by_class_name('ui-state-default')
-    # print(dates_elements)
-    print(len(dates_elements))
-    month_dates_yayornay = []
+                # Search for item in list from indexPos to the end of list
+                indexPos = month_dates_yayornay.index(None, indexPos)
+                # Add the index position in list
+                indexPosList.append(indexPos)
+                indexPos += 1
+            except ValueError:
+                break
 
-    for i in dates_elements:
-        x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a //////find_element_by_css_selector('a').
-        print(x)
-        month_dates_yayornay.append(x)
-    for x in month_dates_yayornay:
-        if x:
-            Dates.append(year + '-' + 'April-' + str(int(month_dates_yayornay.index(x))+1))
-    print(Dates)
+        print(indexPosList)
+        for i in range(len(indexPosList)):
+            if i not in indexPosList:
+                Dates.append(year + '-' + month + '-' + str(int(i)+1))
+        print(Dates)
 
-####### same
-    select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
-    select_month = Select(select_month_button)
-    sleep(2)
-    # select by visible text
-    select_month.select_by_visible_text('May')
-    sleep(5)
-    month = ''
-    dates_elements = browser.find_elements_by_class_name('ui-state-default')
-    # print(dates_elements)
-    print(len(dates_elements))
-    month_dates_yayornay = []
 
-    for i in dates_elements:
-        x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a 
-        print(x)
-        month_dates_yayornay.append(x)
-    print(month_dates_yayornay)
-    # for y in month_dates_yayornay:
-    #     if y != 'None':
-    #         print(month_dates_yayornay.index(y))
-    #         fockinglist.append(y)
-    #         Dates.append(year + '-' + 'May-' + str(int(month_dates_yayornay.index(y))+1))
-    #         print(Dates)
-
-# index only finds first occurence of value that's why I only get 0 and 3
-    indexPosList = []
-    indexPos = 0
-    while True:
-        try:
-            # Search for item in list from indexPos to the end of list
-            indexPos = month_dates_yayornay.index(None, indexPos)
-            # Add the index position in list
-            indexPosList.append(indexPos)
-            indexPos += 1
-        except ValueError:
+        if int(month) <12:
+            month = str('0' + str((int(month) + 1)))
+            print(month)
+            if len(str(month)) == 3:
+                month = month[1:]
+                print(month)
+            letter_month = months.get(month)
+            print(letter_month)
+        elif int(month) == 12:
             break
 
-    print(indexPosList)
-    for i in range(len(indexPosList)):
-        if i not in indexPosList:
-            Dates.append(year + '-' + 'May-' + str(int(i)+1))
-    print(Dates)
+##############################################
+####### same
+#     select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
+#     select_month = Select(select_month_button)
+#     sleep(2)
+#     # select by visible text
+#     select_month.select_by_visible_text('May')
+#     sleep(5)
+#     month = ''
+#     dates_elements = browser.find_elements_by_class_name('ui-state-default')
+#     # print(dates_elements)
+#     print(len(dates_elements))
+#     month_dates_yayornay = []
+
+#     for i in dates_elements:
+#         x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a 
+#         print(x)
+#         month_dates_yayornay.append(x)
+#     print(month_dates_yayornay)
+#     # for y in month_dates_yayornay:
+#     #     if y != 'None':
+#     #         print(month_dates_yayornay.index(y))
+#     #         fockinglist.append(y)
+#     #         Dates.append(year + '-' + 'May-' + str(int(month_dates_yayornay.index(y))+1))
+#     #         print(Dates)
+
+# # index only finds first occurence of value that's why I only get 0 and 3
+#     indexPosList = []
+#     indexPos = 0
+#     while True:
+#         try:
+#             # Search for item in list from indexPos to the end of list
+#             indexPos = month_dates_yayornay.index(None, indexPos)
+#             # Add the index position in list
+#             indexPosList.append(indexPos)
+#             indexPos += 1
+#         except ValueError:
+#             break
+
+#     print(indexPosList)
+#     for i in range(len(indexPosList)):
+#         if i not in indexPosList:
+#             Dates.append(year + '-' + 'May-' + str(int(i)+1))
+#     print(Dates)
 
 
 
