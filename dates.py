84c12fe492bd668041c4beff253b99fe406b226c
George Pt <angry_sephiroth@hotmail.com>
2020-06-05 01:08:56 +0300

    Update dates.py

--- dates.py
+++ dates.py
@@ -86,115 +86,124 @@ def download_comic_with_Selenium():
     # dict with months
 
     #####
-    # year = '2015'
-    select_year.select_by_visible_text(year)
-    sleep(1)
-    while int(month) <13:
-        select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
-        select_month = Select(select_month_button)
-        sleep(2)
-        # select by visible text
-        letter_month = months.get(month)
-        print(letter_month)
-        select_month.select_by_visible_text(letter_month)
-        # try:
-        #     select_month.select_by_visible_text('January')
-        #     month = 'January'
-        # except:
-        #     try:
-        #         select_month.select_by_visible_text('February')
-        #         month = 'February'
-        #     except:
-        #         try:
-        #             select_month.select_by_visible_text('March')
-        #             month = 'March'
-        #         except:
-        #             try:
-        #                 select_month.select_by_visible_text('April')
-        #                 month = 'April'
-        #             except:
-        #                 try:
-        #                     select_month.select_by_visible_text('May')
-        #                     month = 'May'
-        #                 except:
-        #                     try:
-        #                         select_month.select_by_visible_text('June')
-        #                         month = 'June'
-        #                     except:
-        #                         try:
-        #                             select_month.select_by_visible_text('July')
-        #                             month = 'July'
-        #                         except:
-        #                             try:
-        #                                 select_month.select_by_visible_text('August')
-        #                                 month = 'August'
-        #                             except:
-        #                                 try:
-        #                                     select_month.select_by_visible_text('September')
-        #                                     month = 'September'
-        #                                 except:
-        #                                     try:
-        #                                         select_month.select_by_visible_text('October')
-        #                                         month = 'October'
-        #                                     except:
-        #                                         try:
-        #                                             select_month.select_by_visible_text('November')
-        #                                             month = 'November'
-        #                                         except:
-        #                                             select_month.select_by_visible_text('December')
-        #                                             month = 'December'
-                                                
+    while int(year) < 2021:
         sleep(5)
-        print(month)
-        # if letter_month in back_months.keys():
-        #     month = back_months.get(letter_month)
-        #     print(month)
-        dates_elements = browser.find_elements_by_class_name('ui-state-default')
-
-        # print(dates_elements)
-        print(len(dates_elements))
-        month_dates_yayornay = []
-
-        for i in dates_elements:
-            x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a //////find_element_by_css_selector('a').
-            print(x)
-            month_dates_yayornay.append(x)
-        # for x in month_dates_yayornay:
-        #     if x:
-        #         Dates.append(year + '-' + month + '-' + str(int(month_dates_yayornay.index(x))+1))
-        # print(Dates)
-
-
-        # index only finds first occurence of value that's why I only get 0 and 3
-        indexPosList = []
-        indexPos = 0
-        while True:
-            try:
-                # Search for item in list from indexPos to the end of list
-                indexPos = month_dates_yayornay.index(None, indexPos)
-                # Add the index position in list
-                indexPosList.append(indexPos)
-                indexPos += 1
-            except ValueError:
-                break
-
-        print(indexPosList)
-        for i in range(len(indexPosList)):
-            if i not in indexPosList:
-                Dates.append(year + '-' + month + '-' + str(int(i)+1))
-        print(Dates)
-
-
-        if int(month) <12:
-            month = str('0' + str((int(month) + 1)))
-            print(month)
-            if len(str(month)) == 3:
-                month = month[1:]
-                print(month)
+        try:
+            select_year.select_by_visible_text(year)
+        except:
+            sleep(5)
+            select_year.select_by_visible_text(year)
+            sleep(3)
+        while int(month) <13:
+            select_month_button = browser.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/div/div[1]/select')
+            select_month = Select(select_month_button)
+            sleep(2)
+            # select by visible text
             letter_month = months.get(month)
             print(letter_month)
-        elif int(month) == 12:
-            break
+            select_month.select_by_visible_text(letter_month)
+            # try:
+            #     select_month.select_by_visible_text('January')
+            #     month = 'January'
+            # except:
+            #     try:
+            #         select_month.select_by_visible_text('February')
+            #         month = 'February'
+            #     except:
+            #         try:
+            #             select_month.select_by_visible_text('March')
+            #             month = 'March'
+            #         except:
+            #             try:
+            #                 select_month.select_by_visible_text('April')
+            #                 month = 'April'
+            #             except:
+            #                 try:
+            #                     select_month.select_by_visible_text('May')
+            #                     month = 'May'
+            #                 except:
+            #                     try:
+            #                         select_month.select_by_visible_text('June')
+            #                         month = 'June'
+            #                     except:
+            #                         try:
+            #                             select_month.select_by_visible_text('July')
+            #                             month = 'July'
+            #                         except:
+            #                             try:
+            #                                 select_month.select_by_visible_text('August')
+            #                                 month = 'August'
+            #                             except:
+            #                                 try:
+            #                                     select_month.select_by_visible_text('September')
+            #                                     month = 'September'
+            #                                 except:
+            #                                     try:
+            #                                         select_month.select_by_visible_text('October')
+            #                                         month = 'October'
+            #                                     except:
+            #                                         try:
+            #                                             select_month.select_by_visible_text('November')
+            #                                             month = 'November'
+            #                                         except:
+            #                                             select_month.select_by_visible_text('December')
+            #                                             month = 'December'
+                                                    
+            sleep(5)
+            print(month)
+            # if letter_month in back_months.keys():
+            #     month = back_months.get(letter_month)
+            #     print(month)
+            dates_elements = browser.find_elements_by_class_name('ui-state-default')
+
+            # print(dates_elements)
+            print(len(dates_elements))
+            month_dates_yayornay = []
+
+            for i in dates_elements:
+                x = i.get_attribute('href') # #ui-datepicker-div > table > tbody > tr:nth-child(2) > td.undefined > a //////find_element_by_css_selector('a').
+                print(x)
+                month_dates_yayornay.append(x)
+            # for x in month_dates_yayornay:
+            #     if x:
+            #         Dates.append(year + '-' + month + '-' + str(int(month_dates_yayornay.index(x))+1))
+            # print(Dates)
+
+
+            # index only finds first occurence of value that's why I only get 0 and 3
+            indexPosList = []
+            indexPos = 0
+            while True:
+                try:
+                    # Search for item in list from indexPos to the end of list
+                    indexPos = month_dates_yayornay.index(None, indexPos)
+                    # Add the index position in list
+                    indexPosList.append(indexPos)
+                    indexPos += 1
+                except ValueError:
+                    break
+
+            print(indexPosList)
+            for i in range(len(indexPosList)):
+                if i not in indexPosList:
+                    Dates.append(year + '-' + month + '-' + str(int(i)+1))
+            print(Dates)
+
+
+            if int(month) <12:
+                month = str('0' + str((int(month) + 1)))
+                print(month)
+                if len(str(month)) == 3:
+                    month = month[1:]
+                    print(month)
+                letter_month = months.get(month)
+                print(letter_month)
+            elif int(month) == 12:
+                break
+
+        year = str(int(year) + 1)
+        print(year)
+
 
 ##############################################
 ####### same
